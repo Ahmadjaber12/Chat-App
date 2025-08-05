fe0a52bd0f75b42d19e43fb1a9ff90e237193ee5
AhmadJaber12 <zalatahmad87@gmail.com>
2025-08-05 14:20:05 +0300

    Adding Scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import { connection } from './lib/db.js'
 import { errorHandler, notFound } from './middlewares/errorHandling.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 import messageRoute from './routes/messages.route.js'
 import { app, server } from './lib/socket.js'
 dotenv.config()
 connection();
+const __dirname=path.resolve();
 const port = process.env.PORT
 app.use(express.json());
 app.use(cors(
@@ -17,7 +19,15 @@ app.use(cors(
 app.use(cookieParser())
 app.use("/api/auth",authRoutes);
 app.use("/api",messageRoute);
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
+        app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+        app.get('*',(req,res)=>{
+            res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
+        })
+
 app.use(notFound);
 app.use(errorHandler);
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
